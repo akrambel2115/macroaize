29a1dbf0f89b3cefc5aa91054120a494e7835e7d
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-12 17:54:46 +0100

    Refactor LanguageController for clarity and simplicity

--- lib/screens/languageScreen/languageController.dart
+++ lib/screens/languageScreen/languageController.dart
@@ -1,126 +1,59 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../SharePrefHelper/SharePrefKey.dart';
 
-class LanguageController extends GetxController{
-
-  int selectedIndex = 0;
-  String language = "";
+class LanguageController extends GetxController {
+  RxInt selectedIndex = 0.obs;
+  
+  // Language data
+  List<String> languageList = ["English", "French", "Arabic"];
+  List<String> languageCode = ["en", "fr", "ar"];
+  List<String> countryCode = ["US", "FR", "SA"];
+  List<Locale> local = [
+    Locale('en', 'US'),
+    Locale('fr', 'FR'),
+    Locale('ar', 'SA'),
+  ];
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
-    getIndex();
+    getCurrentLanguage();
   }
-  List<String> languageList = [
-    'English',
-    'Turkish',
-    'Spanish',
-    'French',
-    'German',
-    'Chinese',
-    'Russian',
-    'Hindi',
-    'Arabic',
-    'Portuguese',
-    'Indonesian',
-    'Dutch',
-    'Italian',
-    'Polish',
-    'Japanese'
-  ];
-  List local = const  [
-    Locale('en','US'),
-    Locale('turk','TURK'),
-    Locale('sp','SP'),
-    Locale('fre','FRE'),
-    Locale('gem','GEM'),
-    Locale('ch','CH'),
-    Locale('russ','RUSS'),
-    Locale('hi','IN'),
-    Locale('ar','AR'),
-    Locale('por','PORTU'),
-    Locale('indo','INDO'),
-    Locale('dutch','DUTCH'),
-    Locale('ity','ITY'),
-    Locale('polish','POLISH'),
-    Locale('japa','JAPA'),
-  ];
-  List<String> languageCode = [
-    'en',
-    'turk',
-    'sp',
-    'fre',
-    'gem',
-    'ch',
-    'russ',
-    'hi',
-    'ar',
-    'por',
-    'indo',
-    'dutch',
-    'ity',
-    'polish',
-    'japa',
-  ];
 
-  List<String> countryCode = [
-    'US',
-    'TURK',
-    'SP',
-    'FRE',
-    'GEM',
-    'CH',
-    'RUSS',
-    'IN',
-    'AR',
-    'PORTU',
-    'INDO',
-    'DUTCH',
-    'ITY',
-    'POLISH',
-    'JAPA'
-  ];
-  onChangeIndex (int index){
-    selectedIndex = index;
-    storeIndex(selectedIndex);
-    update();
-  }
-  storeIndex(int index) async {
+  void getCurrentLanguage() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setInt('selectedIndex', index);
+    String currentLanguage = prefs.getString(SharePrefKey.language) ?? "English";
+    
+    int index = languageList.indexOf(currentLanguage);
+    if (index != -1) {
+      selectedIndex.value = index;
+    }
     update();
   }
-  getIndex() async {
+
+  void onChangeIndex(int index) {
+    selectedIndex.value = index;
+    update();
+  }
+
+  void storeCountryCode(String code) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    language = prefs.getString(SharePrefKey.language) ?? "English";
-    selectedIndex = prefs.getInt('selectedIndex') ?? 0;
-    update();
+    await prefs.setString(SharePrefKey.countryCode, code);
   }
-  storeLanguageCode(String code) async {
+
+  void storeLanguageCode(String code) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(SharePrefKey.languageCode, code);
-    print("Language code -----> $code");
-    update();
+    await prefs.setString(SharePrefKey.languageCode, code);
   }
-  storeLanguage(int index) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(SharePrefKey.language, languageList[index]);
-    update();
-  }
-  storeCountryCode(String code) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('countryCode', code);
-    print("Country code -----> $code");
-    update();
-  }
-  updateLanguage(Locale locale){
+
+  void updateLanguage(Locale locale) {
     Get.updateLocale(locale);
-    update();
   }
 
+  void storeLanguage(int index) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(SharePrefKey.language, languageList[index]);
+  }
 }
