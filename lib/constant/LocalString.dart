f0350eccb54929c7e8212c238b511aea5d179569
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-12 17:54:18 +0100

    Remove unused language imports and mappings

--- lib/constant/LocalString.dart
+++ lib/constant/LocalString.dart
@@ -1,19 +1,7 @@
 import 'package:get/get.dart';
 import '../LanguageJson/Arabic.dart';
-import '../LanguageJson/Dutch.dart';
 import '../LanguageJson/English.dart';
 import '../LanguageJson/French.dart';
-import '../LanguageJson/German.dart';
-import '../LanguageJson/Hindi.dart';
-import '../LanguageJson/Indonesia.dart';
-import '../LanguageJson/Italian.dart';
-import '../LanguageJson/Japanese.dart';
-import '../LanguageJson/Polish.dart';
-import '../LanguageJson/Portugal.dart';
-import '../LanguageJson/Russian.dart';
-import '../LanguageJson/Spanish.dart';
-import '../LanguageJson/chinese.dart';
-import '../LanguageJson/turkish.dart';
 
 class LocalString extends Translations {
   @override
@@ -21,18 +9,6 @@ class LocalString extends Translations {
     // Arabic first (default + fallback)
     'ar_AR': arabic,
     'en_US':  english,
-    'japa_JAPA': japanese,
-    'turk_TURK': turkish,
-    'polish_POLISH': polish,
-    'ity_ITY': italian,
-    'dutch_DUTCH': dutch,
-    'indo_INDO': indonesia,
-    'por_PORTU': portugal,
-    'hi_IN': hindi,
-    'russ_RUSS': russian,
-    'ch_CH': chinese,
-    'gem_GEM': german,
     'fre_FRE': french,
-    'sp_SP': spanish
   };
 }
