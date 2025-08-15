4057d5c07f61263e82f82f390dfe3b95fe6ffa1f
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-15 03:15:13 +0100

    Add modern animation widgets

---
+++ lib/widgets/ModernAnimations.dart
@@ -0,0 +1,482 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../constant/AppColor.dart';
+
+class ModernLoadingIndicator extends StatefulWidget {
+  final double size;
+  final Color? color;
+  final double strokeWidth;
+  final Duration duration;
+
+  const ModernLoadingIndicator({
+    Key? key,
+    this.size = 40,
+    this.color,
+    this.strokeWidth = 3,
+    this.duration = const Duration(milliseconds: 1500),
+  }) : super(key: key);
+
+  @override
+  State<ModernLoadingIndicator> createState() => _ModernLoadingIndicatorState();
+}
+
+class _ModernLoadingIndicatorState extends State<ModernLoadingIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _strokeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+
+    _rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.linear,
+    ));
+
+    _strokeAnimation = Tween<double>(
+      begin: 0.1,
+      end: 0.9,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = widget.color ?? context.theme.focusColor;
+
+    return SizedBox(
+      width: widget.size,
+      height: widget.size,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: _rotationAnimation.value * 2 * 3.14159,
+            child: CustomPaint(
+              painter: _ModernLoadingPainter(
+                strokeWidth: widget.strokeWidth,
+                color: color,
+                progress: _strokeAnimation.value,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _ModernLoadingPainter extends CustomPainter {
+  final double strokeWidth;
+  final Color color;
+  final double progress;
+
+  _ModernLoadingPainter({
+    required this.strokeWidth,
+    required this.color,
+    required this.progress,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = (size.width - strokeWidth) / 2;
+
+    // Background circle
+    final backgroundPaint = Paint()
+      ..color = color.withOpacity(0.1)
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawCircle(center, radius, backgroundPaint);
+
+    // Progress arc
+    final progressPaint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    final sweepAngle = 2 * 3.14159 * progress;
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -3.14159 / 2, // Start from top
+      sweepAngle,
+      false,
+      progressPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class ModernShimmerLoading extends StatefulWidget {
+  final Widget child;
+  final bool isLoading;
+  final Color? baseColor;
+  final Color? highlightColor;
+  final Duration duration;
+
+  const ModernShimmerLoading({
+    Key? key,
+    required this.child,
+    required this.isLoading,
+    this.baseColor,
+    this.highlightColor,
+    this.duration = const Duration(milliseconds: 1500),
+  }) : super(key: key);
+
+  @override
+  State<ModernShimmerLoading> createState() => _ModernShimmerLoadingState();
+}
+
+class _ModernShimmerLoadingState extends State<ModernShimmerLoading>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    _animation = Tween<double>(
+      begin: -1.0,
+      end: 2.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    if (widget.isLoading) {
+      _controller.repeat();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ModernShimmerLoading oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isLoading && !oldWidget.isLoading) {
+      _controller.repeat();
+    } else if (!widget.isLoading && oldWidget.isLoading) {
+      _controller.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) {
+      return widget.child;
+    }
+
+    final isDark = context.theme.brightness == Brightness.dark;
+    final baseColor = widget.baseColor ?? 
+        (isDark ? AppColor.darkCard : AppColor.neutralGrey200);
+    final highlightColor = widget.highlightColor ?? 
+        (isDark ? AppColor.darkBorder : AppColor.neutralGrey100);
+
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return ShaderMask(
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                baseColor,
+                highlightColor,
+                baseColor,
+              ],
+              stops: [
+                0.0,
+                0.5,
+                1.0,
+              ],
+              transform: GradientRotation(_animation.value),
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class ModernPulseAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final double minScale;
+  final double maxScale;
+  final bool repeat;
+
+  const ModernPulseAnimation({
+    Key? key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 1000),
+    this.minScale = 0.95,
+    this.maxScale = 1.05,
+    this.repeat = true,
+  }) : super(key: key);
+
+  @override
+  State<ModernPulseAnimation> createState() => _ModernPulseAnimationState();
+}
+
+class _ModernPulseAnimationState extends State<ModernPulseAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(
+      begin: widget.minScale,
+      end: widget.maxScale,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    if (widget.repeat) {
+      _controller.repeat(reverse: true);
+    } else {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _scaleAnimation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _scaleAnimation.value,
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class ModernFadeSlideTransition extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Offset beginOffset;
+  final Offset endOffset;
+  final bool slideIn;
+
+  const ModernFadeSlideTransition({
+    Key? key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 600),
+    this.beginOffset = const Offset(0, 0.3),
+    this.endOffset = Offset.zero,
+    this.slideIn = true,
+  }) : super(key: key);
+
+  @override
+  State<ModernFadeSlideTransition> createState() => _ModernFadeSlideTransitionState();
+}
+
+class _ModernFadeSlideTransitionState extends State<ModernFadeSlideTransition>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: widget.slideIn ? 0.0 : 1.0,
+      end: widget.slideIn ? 1.0 : 0.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _slideAnimation = Tween<Offset>(
+      begin: widget.slideIn ? widget.beginOffset : widget.endOffset,
+      end: widget.slideIn ? widget.endOffset : widget.beginOffset,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return FadeTransition(
+          opacity: _fadeAnimation,
+          child: SlideTransition(
+            position: _slideAnimation,
+            child: widget.child,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ModernScaleTransition extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final double initialScale;
+  final double finalScale;
+  final Curve curve;
+
+  const ModernScaleTransition({
+    Key? key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 200),
+    this.initialScale = 0.8,
+    this.finalScale = 1.0,
+    this.curve = Curves.easeOutBack,
+  }) : super(key: key);
+
+  @override
+  _ModernScaleTransitionState createState() => _ModernScaleTransitionState();
+}
+
+class _ModernScaleTransitionState extends State<ModernScaleTransition>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    
+    _scaleAnimation = Tween<double>(
+      begin: widget.initialScale,
+      end: widget.finalScale,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: widget.curve,
+    ));
+    
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _scaleAnimation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _scaleAnimation.value,
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class ModernSkeletonLoader extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final BorderRadius? borderRadius;
+  final bool isCircle;
+
+  const ModernSkeletonLoader({
+    Key? key,
+    this.width,
+    this.height,
+    this.borderRadius,
+    this.isCircle = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = context.theme.brightness == Brightness.dark;
+    final baseColor = isDark ? AppColor.darkCard : AppColor.neutralGrey200;
+    final highlightColor = isDark ? AppColor.darkBorder : AppColor.neutralGrey100;
+
+    Widget skeleton = Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: baseColor,
+        borderRadius: isCircle 
+          ? null 
+          : (borderRadius ?? BorderRadius.circular(8)),
+        shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+      ),
+    );
+
+    return ModernShimmerLoading(
+      isLoading: true,
+      baseColor: baseColor,
+      highlightColor: highlightColor,
+      child: skeleton,
+    );
+  }
+}
