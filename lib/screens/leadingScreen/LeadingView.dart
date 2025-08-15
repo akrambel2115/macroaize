87198f0bb5f5745dea87f53d91d73ff30fa0387f
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-15 03:12:24 +0100

    Refactor bottom navigation and chat button UI

--- lib/screens/leadingScreen/LeadingView.dart
+++ lib/screens/leadingScreen/LeadingView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodcalorietracker/constant/AppAssets.dart';
+import 'package:foodcalorietracker/constant/AppColor.dart';
 import 'package:foodcalorietracker/routes/app_routes.dart';
 import 'package:foodcalorietracker/screens/AnalyticsScreen/AnalyticsView.dart';
 import 'package:foodcalorietracker/screens/HomeScreen/HomeView.dart';
@@ -21,129 +21,83 @@ class LeadingView extends GetView<LeadingController> {
       },
       child: Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton:  FloatingActionButton(onPressed: () {
-          print("Hello on Tap");
-          Get.toNamed(Routes.chatView);
-        },
-          backgroundColor: context.theme.focusColor,
-          shape: CircleBorder(),
-          child:Icon(Icons.chat,color: context.theme.scaffoldBackgroundColor,size: 30,),
+        floatingActionButton: GetBuilder<LeadingController>(
+          builder: (controller) {
+            // Hide chat button when scanner is active for distraction-free experience
+            if (controller.currentIndex == 1) {
+              return const SizedBox.shrink();
+            }
+            
+            return FloatingActionButton(
+              onPressed: () {
+                Get.toNamed(Routes.chatView);
+              },
+              backgroundColor: context.theme.focusColor,
+              shape: const CircleBorder(),
+              elevation: 6,
+              child: Icon(
+                Icons.chat_rounded,
+                color: context.theme.scaffoldBackgroundColor,
+                size: 28,
+              ),
+            );
+          },
         ),
           bottomNavigationBar: GetBuilder<LeadingController>(
             builder: (controller) {
-              return SizedBox(
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  // space items evenly
-                  children: [
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          controller.changeTabIndex(0);
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.home,
-                              color: controller.currentIndex == 0
-                                  ? context.theme.focusColor
-                                  : Colors.grey,
-                            ),
-                            Text(
-                              'Home'.tr,
-                              style: TextStyle(
-                                color: controller.currentIndex == 0
-                                    ? context.theme.focusColor
-                                    : Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          controller.changeTabIndex(1);
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              AppAssets.scanHomeIcon,
-                              fit: BoxFit.fill,
-                              height: 22,
-                              width: 22,
-                              color: controller.currentIndex == 1
-                                  ? context.theme.focusColor
-                                  : Colors.grey,
-                            ),
-                            Text('Calorie'.tr,
-                                style: TextStyle(
-                                  color: controller.currentIndex == 1
-                                      ? context.theme.focusColor
-                                      : Colors.grey,
-                                )),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          controller.changeTabIndex(2);
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-
-                            Icon(
-                              Icons.analytics_outlined,
-                              color: controller.currentIndex == 2
-                                  ? context.theme.focusColor
-                                  : Colors.grey,
-                            ),
-                            Text('Analytics'.tr,
-                                style: TextStyle(
-                                  color: controller.currentIndex == 2
-                                      ? context.theme.focusColor
-                                      : Colors.grey,
-                                )),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          controller.changeTabIndex(3);
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.settings,
-
-                              color: controller.currentIndex == 3
-                                  ? context.theme.focusColor
-                                  : Colors.grey,
-                            ),
-                            Text('Setting'.tr,
-                                style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  color: controller.currentIndex == 3
-                                      ? context.theme.focusColor
-                                      : Colors.grey,
-                                )),
-                          ],
-                        ),
-                      ),
+              return Container(
+                height: 80 + MediaQuery.of(context).padding.bottom,
+                decoration: BoxDecoration(
+                  color: context.theme.scaffoldBackgroundColor,
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColor.lightShadow,
+                      blurRadius: 12,
+                      offset: const Offset(0, -4),
                     ),
                   ],
                 ),
-              ).paddingOnly(bottom: MediaQuery.of(context).padding.bottom+5);
+                child: SafeArea(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildNavItem(
+                        context, 
+                        controller, 
+                        0, 
+                        Icons.dashboard_rounded, 
+                        Icons.dashboard_outlined, 
+                        'Home'.tr
+                      ),
+                      _buildNavItem(
+                        context, 
+                        controller, 
+                        1, 
+                        Icons.document_scanner_rounded, 
+                        Icons.document_scanner_outlined, 
+                        'Scanner'.tr
+                      ),
+                      _buildNavItem(
+                        context, 
+                        controller, 
+                        2, 
+                        Icons.insights_rounded, 
+                        Icons.insights_outlined, 
+                        'Analytics'.tr
+                      ),
+                      _buildNavItem(
+                        context, 
+                        controller, 
+                        3, 
+                        Icons.person_rounded, 
+                        Icons.person_outline_rounded, 
+                        'Settings'.tr
+                      ),
+                    ],
+                  ),
+                ),
+              );
             },
           ),
 
@@ -160,6 +114,65 @@ class LeadingView extends GetView<LeadingController> {
               }
             },
           )),
+    );
+  }
+
+  Widget _buildNavItem(
+    BuildContext context,
+    LeadingController controller,
+    int index,
+    IconData activeIcon,
+    IconData inactiveIcon,
+    String label,
+  ) {
+    final isSelected = controller.currentIndex == index;
+    
+    return Expanded(
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () => controller.changeTabIndex(index),
+          borderRadius: BorderRadius.circular(16),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: isSelected 
+                      ? context.theme.focusColor.withOpacity(0.1)
+                      : Colors.transparent,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Icon(
+                    isSelected ? activeIcon : inactiveIcon,
+                    color: isSelected
+                        ? context.theme.focusColor
+                        : AppColor.neutralGrey500,
+                    size: 24,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  label,
+                  style: context.textTheme.labelSmall?.copyWith(
+                    color: isSelected
+                        ? context.theme.focusColor
+                        : AppColor.neutralGrey500,
+                    fontWeight: isSelected
+                        ? FontWeight.w600
+                        : FontWeight.w500,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
