c3adcb38076e0559ee80e51f7975519334d9edfb
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-15 03:10:39 +0100

    Add sort order toggle for history list

--- lib/screens/historyScreen/HistoryController.dart
+++ lib/screens/historyScreen/HistoryController.dart
@@ -8,6 +8,8 @@ class HistoryController extends GetxController{
 
   List<CalorieHistoryModel> sqlHistory = [];
   String type = "";
+  // false -> newest first (descending by id), true -> oldest first (ascending)
+  bool sortAsc = false;
   final dbHelper = DatabaseHelper();
   @override
   void onInit() {
@@ -20,6 +22,23 @@ class HistoryController extends GetxController{
   getHistory()
   async {
     sqlHistory =  await dbHelper.getCalorieHistory(type);
+    // apply current sort preference
+    if (sqlHistory.isNotEmpty) {
+      sqlHistory.sort((a, b) => sortAsc
+          ? (a.id ?? 0).compareTo(b.id ?? 0)
+          : (b.id ?? 0).compareTo(a.id ?? 0));
+    }
+    update();
+  }
+
+  /// Toggle the list sort order and refresh the view.
+  void toggleSort() {
+    sortAsc = !sortAsc;
+    if (sqlHistory.isNotEmpty) {
+      sqlHistory.sort((a, b) => sortAsc
+          ? (a.id ?? 0).compareTo(b.id ?? 0)
+          : (b.id ?? 0).compareTo(a.id ?? 0));
+    }
     update();
   }
 
