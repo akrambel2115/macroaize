3135d4870bbd740957c50c34135df0ac75a4ebe1
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-08-21 23:21:13 +0100

    Add AppFonts constant class

---
+++ lib/constant/AppFonts.dart
@@ -0,0 +1,5 @@
+class AppFonts {
+  // Change `splashFont` to any font family available in pubspec.yaml
+  // For an "icon style" look, replace this with the family name of your icon font after adding it to assets.
+  static const String splashFont = 'Poppins-SemiBold';
+}
