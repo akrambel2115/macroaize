9aee078a4d927bc5c8657399e7e592e071e15dea
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-12-01 16:17:36 +0100

    Add home widget providers and update build config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.macroaize.app"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.macroaize.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
